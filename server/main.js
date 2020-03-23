25ba095821d50d404d0e348f9a79fc79b5ca36e8
ContainedBlargh <jon@zendata.dk>
2020-03-23 22:42:21 +0100

    add listening on specific addressses

--- server/main.js
+++ server/main.js
@@ -2,11 +2,10 @@ const express = require('express')
 const config = require('./config')
 const path = require('path')
 const app = express()
-const port = config.port
 
 const main = async () => {
     app.use(express.static(path.join(__dirname, '../app/')))
-    app.listen(port, () => console.log(`Listening on port ${port}`))
+    app.listen(config.port, config.address, () => console.log(`Listening on port ${port}`))
 }
 
 main()
